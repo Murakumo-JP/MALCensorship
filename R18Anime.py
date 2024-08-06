ca2bcf9087905f7612941173b2ed59b8d1532fb8
Murakumo <32967450+Murakumo-JP@users.noreply.github.com>
2024-08-06 21:31:07 +0000

    Up Code v2

--- R18Anime.py
+++ R18Anime.py
@@ -21,6 +21,7 @@ headers = {
 }
 
 while True:
+    print(f"Fetching page {page}...")
     request = requests.get(f"https://myanimelist.net/anime/genre/12/Hentai?page={page}", headers=headers)
     soup = BeautifulSoup(request.text, 'html.parser')
     links = soup.find_all("a", class_="link-title")
@@ -28,6 +29,7 @@ while True:
     if links:
         page += 1
     else:
+        print("No more pages found. Exiting.")
         break
 
     for link in links:
@@ -36,6 +38,7 @@ while True:
         new_entry = f'.data.image a[href^="{temp}/"]:before{{content: var(--R18);}}'
         
         if new_entry not in existing_entries:
+            print(f"Adding new entry: {new_entry}")
             animelist.append(new_entry)
             existing_entries.add(new_entry)
 
@@ -43,4 +46,4 @@ with open(file_path, 'a') as f:
     for index in animelist:
         f.write(index + '\n')
 
-print(animelist)
+print("Scraping completed. New entries added to R18Anime.css")
