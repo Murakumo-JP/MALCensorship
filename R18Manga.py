0b66dc7a8093d1fe0a835b4d526770764542935a
Murakumo <32967450+Murakumo-JP@users.noreply.github.com>
2024-08-06 21:09:33 +0000

    Action Manga Test

--- R18Manga.py
+++ R18Manga.py
@@ -7,8 +7,14 @@ animelist = []
 page = 1
 
 file_path = 'R18Manga.css'
+
 if not os.path.exists(file_path):
     open(file_path, 'w').close()
+
+existing_entries = set()
+with open(file_path, 'r') as f:
+    for line in f:
+        existing_entries.add(line.strip())
 
 while True:
     request = requests.get(f"https://myanimelist.net/manga/genre/12/Hentai?page={page}")
@@ -23,9 +29,13 @@ while True:
     for link in links:
         temp = re.sub(r'(?:.*?myanimelist\.net)', '', link.get('href'))
         temp = re.sub(r'\/[^\/]*?$', '', temp)
-        animelist.append(f'.data.image a[href^="{temp}/"]:before{{content: var(--R18);}}')
+        new_entry = f'.data.image a[href^="{temp}/"]:before{{content: var(--R18);}}'
+        
+        if new_entry not in existing_entries:
+            animelist.append(new_entry)
+            existing_entries.add(new_entry)
 
-with open(file_path, 'w') as f:
+with open(file_path, 'a') as f:
     for index in animelist:
         f.write(index + '\n')
 
